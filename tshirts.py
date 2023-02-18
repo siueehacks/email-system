b089dd8a91b660a83498b222cd6f439a04f5ec6e
Michael French <michael.x.french@gmail.com>
2023-02-18 09:02:52 -0600

    simplify tshirts.py code

--- tshirts.py
+++ tshirts.py
@@ -1,26 +1,9 @@
-"""Will want to break this out into another repo"""
-
-from __future__ import print_function
-
-import os.path
-import os
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
 
-from helpers.utils import get_creds
+from helpers.sheets import get_sheet_info
 
 load_dotenv()
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = os.getenv("SHEET_ID")
 SAMPLE_RANGE_NAME = "Form Responses 1!L:L"
 
 
@@ -28,35 +11,21 @@ def main():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    creds = get_creds()
+    values = get_sheet_info(SAMPLE_RANGE_NAME)
 
-    try:
-        service = build("sheets", "v4", credentials=creds)
+    if not values:
+        print("No data found.")
+        return
 
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = (
-            sheet.values()
-            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-            .execute()
-        )
-        values = result.get("values", [])
-
-        if not values:
-            print("No data found.")
-            return
-
-        shirt_dict = {}
-        for row in values[1:]:
-            if row[0] in shirt_dict:
-                shirt_dict[row[0]] += 1
-            else:
-                shirt_dict[row[0]] = 1
-        
-        print(shirt_dict)
-
-    except HttpError as err:
-        print(err)
+    shirt_dict = {}
+    for row in values[1:]:
+        if row[0] in shirt_dict:
+            shirt_dict[row[0]] += 1
+        else:
+            shirt_dict[row[0]] = 1
+    
+    print(f'Total: {len(values[1:])}')
+    print(shirt_dict)
 
 
 if __name__ == "__main__":
